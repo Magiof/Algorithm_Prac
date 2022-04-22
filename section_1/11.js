3816a1acaae6930f8991211e9739cb8a2a4c1ffb
unknown <tjddnjs7559@gmail.com>
2022-04-22 23:03:41 +0900

    FIX: another method

--- section_1/11.js
+++ section_1/11.js
@@ -7,5 +7,15 @@ function solution(s) {
   return answer;
 }
 
+//아스키코드 넘버를 이용
+//대문자: 65~90, 소문자: 97~122
+function solution2(s) {
+  let answer = 0;
+  for (let x of s) {
+    let num = x.charCodeAt(); //아스키코드로 변환
+    if (num >= 65 && num <= 90) answer++;
+  }
+}
 let str = "KoreaTimeGood";
 console.log(solution(str));
+console.log(solution2(str));
