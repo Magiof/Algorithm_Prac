67a16a66be12fc935e8ae515fddd4778101b08e7
unknown <tjddnjs7559@gmail.com>
2022-04-22 15:24:05 +0900

    FIX: min method

--- section_1/5.js
+++ section_1/5.js
@@ -1,12 +1,18 @@
 // 7개의 수가 주어지면 그 숫자 중 최솟값 구하기
 
-function solution(arr) {
-  let min = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < min) min = arr[i];
-  }
-  return min;
-}
+// function solution(arr) {
+//   let min = arr[0];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < min) min = arr[i];
+//   }
+//   return min;
+// }
 
+// let arr = [5, 7, 1, 3, 2, 9, 11];
+// console.log(solution(arr));
+
+function solution(arr) {
+  return Math.min(...arr);
+}
 let arr = [5, 7, 1, 3, 2, 9, 11];
 console.log(solution(arr));
