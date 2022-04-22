32fb8bcad4ead8ae274e9b244cc25f8b1bd81e1e
unknown <tjddnjs7559@gmail.com>
2022-04-22 22:43:25 +0900

    FIX: another method

--- section_1/10.js
+++ section_1/10.js
@@ -1,12 +1,17 @@
 // 입력받은 문자열에 특정 문자가 몇개인지 출력
 
-function solution(s, t){
-   let answer=0;
-   for(let x of s){
-       if(x===t) answer++
-   }
-    return answer;
+function solution(s, t) {
+  let answer = 0;
+  for (let x of s) {
+    if (x === t) answer++;
+  }
+  return answer;
 }
 
-let str="COMPUTERPROGRAMMING";
-console.log(solution(str, 'R'));
+function solution2(s, t) {
+  let answer = s.split(t).length;
+  return answer - 1;
+}
+let str = "COMPUTERPROGRAMMING";
+console.log(solution(str, "R"));
+console.log(solution2(str, "R"));
