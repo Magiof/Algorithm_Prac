f5d9f71c79d4cf21d2fe6716da5e4f3942cf06e6
seongwon <87584033+Magiof@users.noreply.github.com>
2022-08-07 23:58:53 +0900

    ADD: 6.2

---
+++ section_6/2.js
@@ -0,0 +1,16 @@
+// 소괄호 ( ) 사이에 존재하는 모든 문자 제거.
+// 남은 문자만 출력
+
+function solution(s) {
+  let answer;
+  let stack = [];
+  for (let x of s) {
+    if (x === ")") {
+      while (stack.pop() !== "(");
+    } else stack.push(x);
+  }
+  return answer;
+}
+
+let s = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+console.log(solution(s));
