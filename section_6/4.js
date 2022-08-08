95658d34a225572f32b4cfbebbd6ac51c3909955
MacBook Air <macbookair@MacBookui-MacBookAir.local>
2022-08-08 14:25:15 +0900

    ADD: 6.4

---
+++ section_6/4.js
@@ -0,0 +1,17 @@
+function solution(s) {
+  let answer;
+  let stack = [];
+
+  for (let x of s) {
+    if (!isNaN(x)) stack.push(Number(x));
+    else {
+      let rt = stack.pop();
+      let lt = stack.pop();
+      stack.push(eval(lt + x + rt));
+    }
+  }
+  answer = stack[0];
+  return answer;
+}
+let str = "352+*9-";
+console.log(solution(str));
