02f151eaa6ba67184cbb382f966c8372c3b918d4
Magiof <tjddnjs7559@gmail.com>
2022-08-12 00:21:13 +0900

    FIX: 7.10

---
+++ section_7/10.js
@@ -0,0 +1,22 @@
+// 이분검색
+// arr에서 n은 몇번째로 작은 수 인가?
+
+function solution(arr, n) {
+  let answer;
+  let tmp = arr.sort((a, b) => a - b);
+  let lt = 0;
+  let rt = arr.length - 1;
+  while (1) {
+    let mid = parseInt((lt + rt) / 2);
+    if (tmp[mid] === n) {
+      answer = mid + 1;
+      break;
+    } else if (tmp[mid] > n) rt = mid;
+    else if (tmp[mid] < n) lt = mid;
+  }
+  return answer;
+}
+
+let arr = [23, 87, 65, 12, 57, 32, 99, 81];
+let n = 32;
+console.log(solution(arr, n));
